a818e7392a911aff22df11d014088ea34f053701
LukaVieira1 <lukavieira14@gmail.com>
2025-01-19 16:35:14 -0300

    add: storeId in token payload

--- backend/src/@types/fastify/index.d.ts
+++ backend/src/@types/fastify/index.d.ts
@@ -3,6 +3,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 declare module "fastify" {
   export interface FastifyRequest {
     user?: {
+      storeId: number;
       id: number;
       role: string;
     };
