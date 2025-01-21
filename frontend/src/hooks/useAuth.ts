ac25b5bec6efb9de23dd7b80064ca91b92104e42
LukaVieira1 <lukavieira14@gmail.com>
2025-01-21 08:29:14 -0300

    add: useAuth hook

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { IUser, IAuthResponse } from "@/types/auth";
+import { login } from "@/services/auth";
+import api from "@/lib/api";
+
+export function useAuth() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [user, setUser] = useState<IUser | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const storedUser = localStorage.getItem("@Legerly:user");
+    const storedToken = localStorage.getItem("@Legerly:token");
+
+    if (storedUser && storedToken) {
+      api.defaults.headers.authorization = `Bearer ${storedToken}`;
+      setUser(JSON.parse(storedUser));
+    } else if (pathname !== "/login") {
+      router.push("/login");
+    }
+
+    setIsLoading(false);
+  }, [pathname, router]);
+
+  async function signIn(
+    email: string,
+    password: string
+  ): Promise<IAuthResponse | Error> {
+    try {
+      const response = await login(email, password);
+
+      localStorage.setItem("@Legerly:token", response.token);
+      localStorage.setItem("@Legerly:user", JSON.stringify(response.user));
+
+      setUser(response.user);
+      router.push("/");
+
+      return response;
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        alert("Ocorreu um erro inesperado");
+      }
+      return error as Error;
+    }
+  }
+
+  function signOut() {
+    localStorage.removeItem("@Legerly:token");
+    localStorage.removeItem("@Legerly:user");
+    delete api.defaults.headers.authorization;
+    setUser(null);
+    router.push("/login");
+  }
+
+  return {
+    user,
+    signIn,
+    signOut,
+    isAuthenticated: !!user,
+    isLoading,
+  };
+}
