a6c21829939d10cd0e2c52182a2e49c943c102e2
LukaVieira1 <lukavieira14@gmail.com>
2025-01-22 08:12:06 -0300

    add: exclamation and dollar icon

---
+++ frontend/src/components/Icons.tsx
@@ -0,0 +1,39 @@
+export function DollarCircleIcon() {
+  return (
+    <div className="bg-primary-50 p-3 rounded-full">
+      <svg
+        className="w-6 h-6 text-primary-600"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export function ExclamationCircleIcon() {
+  return (
+    <div className="bg-warning-50 p-3 rounded-full">
+      <svg
+        className="w-6 h-6 text-warning-600"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </div>
+  );
+}
