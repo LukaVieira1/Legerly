1165a5af2b5548fd921ee5b2eac5bed2e8dfa74e
LukaVieira1 <lukavieira14@gmail.com>
2025-01-22 08:12:38 -0300

    add: metrics with skeleton loading

--- frontend/src/app/(dashboard)/page.tsx
+++ frontend/src/app/(dashboard)/page.tsx
@@ -1,38 +1,76 @@
+// Next
 "use client";
+import { useEffect, useState } from "react";
 
+// Providers
 import { useAuthContext } from "@/providers/AuthProvider";
+
+// Types
+import { IStoreMetrics } from "@/types/store";
+
+// Services
+import { getStoreMetrics } from "@/services/store";
+
+// Components
+import { DollarCircleIcon, ExclamationCircleIcon } from "@/components/Icons";
 import DashboardLayout from "./layout";
+import { MetricCard } from "@/components/MetricCard";
+import { MetricSkeleton } from "@/components/skeletons/MetricSkeleton";
 
 export default function Dashboard() {
   const { user } = useAuthContext();
+  const [metrics, setMetrics] = useState<IStoreMetrics | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchMetrics = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        const metrics = await getStoreMetrics();
+        setMetrics(metrics);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMetrics();
+  }, []);
 
   return (
     <DashboardLayout>
-      <div className="space-y-6">
-        <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold">Dashboard</h1>
-          <div className="text-sm text-gray-500">Bem-vindo, {user?.name}</div>
+      <div className="space-y-8">
+        <div className="bg-white p-6 rounded-lg shadow-sm border border-secondary-200">
+          <h1 className="text-2xl font-medium text-secondary-900">
+            Olá,{" "}
+            <span className="font-bold text-primary-600">{user?.name}</span>
+          </h1>
+          <p className="mt-1 text-secondary-500">
+            Bem-vindo ao painel de controle da {user?.store.name}
+          </p>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
-            <h2 className="text-lg font-semibold text-gray-900">Vendas Hoje</h2>
-            <p className="text-3xl font-bold text-primary-600">R$ 0,00</p>
-          </div>
-
-          <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
-            <h2 className="text-lg font-semibold text-gray-900">
-              Total de Clientes
-            </h2>
-            <p className="text-3xl font-bold text-primary-600">0</p>
-          </div>
-
-          <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
-            <h2 className="text-lg font-semibold text-gray-900">
-              Débitos Pendentes
-            </h2>
-            <p className="text-3xl font-bold text-primary-600">R$ 0,00</p>
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {loading ? (
+            <>
+              <MetricSkeleton />
+              <MetricSkeleton />
+            </>
+          ) : (
+            <>
+              <MetricCard
+                metrics={Number(metrics?.totalPayments)}
+                title="Total de Vendas"
+                icon={<DollarCircleIcon />}
+              />
+              <MetricCard
+                metrics={Number(metrics?.totalDebits)}
+                title="Débitos Pendentes"
+                icon={<ExclamationCircleIcon />}
+              />
+            </>
+          )}
         </div>
       </div>
     </DashboardLayout>
