1c14410612224b6320f793972544190653dd1542
LukaVieira1 <lukavieira14@gmail.com>
2025-01-23 07:07:11 -0300

    add: user types

---
+++ frontend/src/types/client.ts
@@ -0,0 +1,11 @@
+export interface IClient {
+  id: number;
+  name: string;
+  phone: string;
+  birthDate: string;
+  observations: string;
+  debitBalance: string;
+  createdAt: string;
+  updatedAt: string;
+  storeId: number;
+}
