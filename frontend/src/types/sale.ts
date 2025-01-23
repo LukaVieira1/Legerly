46aaf7a9cc981b119b211929502f8240b8a0f3dd
LukaVieira1 <lukavieira14@gmail.com>
2025-01-23 07:50:53 -0300

    add: Sale type

---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,18 @@
+import { IShortUser } from "./user";
+import { IClient } from "./client";
+
+export interface ISale {
+  id: number;
+  value: string;
+  description: string;
+  isPaid: boolean;
+  saleDate: string;
+  dueDate: string;
+  createdAt: string;
+  updatedAt: string;
+  storeId: number;
+  clientId: number;
+  userId: number;
+  client: IClient;
+  user: IShortUser;
+}
