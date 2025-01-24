70fd6fc4be3b11fb5a9bac0834f117d38c8086d2
LukaVieira1 <lukavieira14@gmail.com>
2025-01-24 09:03:58 -0300

    feature: clients page

---
+++ frontend/src/app/(dashboard)/clients/page.tsx
@@ -0,0 +1,79 @@
+// Next
+"use client";
+import { useEffect, useState } from "react";
+
+// Hooks
+import { useClients } from "@/hooks/useClients";
+
+// Types
+import { IClient } from "@/types/client";
+
+// Icons
+import { FiUserPlus } from "react-icons/fi";
+
+// Components
+import { ClientList } from "@/components/ClientList";
+import { ClientModal } from "@/components/ClientModal";
+import { ClientSkeleton } from "@/components/skeletons/ClientSkeleton";
+
+export default function Clients() {
+  const { clients, isLoading } = useClients();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedClient, setSelectedClient] = useState<IClient | null>(null);
+
+  function handleOpenModal(client?: IClient) {
+    if (client) {
+      setSelectedClient(client);
+    } else {
+      setSelectedClient(null);
+    }
+    setIsModalOpen(true);
+  }
+
+  useEffect(() => {
+    console.log(clients);
+  }, [clients]);
+
+  return (
+    <div className="space-y-8">
+      <div className="bg-white p-6 rounded-lg shadow-sm border border-secondary-200">
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="text-2xl font-medium text-secondary-900">
+              Clientes
+            </h1>
+            <p className="mt-1 text-secondary-500">
+              Gerencie os clientes da sua loja
+            </p>
+          </div>
+          <button
+            onClick={() => handleOpenModal()}
+            className="inline-flex items-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+          >
+            <FiUserPlus className="mr-2 h-5 w-5" />
+            Novo Cliente
+          </button>
+        </div>
+      </div>
+
+      {isLoading ? (
+        <div className="space-y-4">
+          {[1, 2, 3].map((n) => (
+            <ClientSkeleton key={n} />
+          ))}
+        </div>
+      ) : (
+        <ClientList clients={clients} onEdit={handleOpenModal} />
+      )}
+
+      <ClientModal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+          setSelectedClient(null);
+        }}
+        client={selectedClient}
+      />
+    </div>
+  );
+}
