bccc70b9d26f371ad6a9637889408ddf6eee24f4
LukaVieira1 <lukavieira14@gmail.com>
2025-02-06 11:03:59 -0300

    fix: build pages error, for vercel deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true,
+    typedRoutes: true,
+  },
+  output: "standalone",
+  trailingSlash: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
